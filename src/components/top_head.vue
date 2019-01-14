528426e5a540263525d4eca12c529b2c71ac4d77
wenyun_kang <798218157@qq.com>
2019-01-14 14:18:22 +0800

    top_head.vue 加入是否已登录的状态切换

--- src/components/top_head.vue
+++ src/components/top_head.vue
@@ -13,7 +13,7 @@
       <el-menu-item index="home">首页</el-menu-item>
       <el-menu-item index="mdd">目的地</el-menu-item>
       <el-menu-item index="raiders">旅游攻略</el-menu-item>
-      <el-submenu index="my" class="my">
+      <el-submenu index="my" class="my" v-show="login">
         <template slot="title"><img src="../assets/user.png"/></template>
         <el-menu-item index="1"><i class="el-icon-loading"></i>金币 {{coin}}</el-menu-item>
         <el-menu-item index="write_raiders"><i class="el-icon-edit"></i>写游记</el-menu-item>
@@ -22,7 +22,7 @@
         <el-menu-item index="quit"><i class="el-icon-d-arrow-right"></i>退出</el-menu-item>
       </el-submenu>
     </el-menu>
-    <div class="right_header">
+    <div class="right_header" v-if="login">
       <div class="find_set">
         <transition name="el-zoom-in-center">
           <el-input
@@ -42,6 +42,14 @@
       </div>
       <el-button class="daka">打卡</el-button>
     </div>
+    <div v-else class="login-out">
+      <a class="weibo-login" href="https://passport.mafengwo.cn/weibo" title="微博登录"></a>
+      <a class="qq-login" href="https://passport.mafengwo.cn/qq" title="QQ登录"></a>
+      <a class="weixin-login" href="https://passport.mafengwo.cn/wechat" title="微信登录"></a>
+      <a title="登录马蜂窝" href="/login" target="_blank">登录</a>
+      <span class="split">|</span>
+      <a href="/login" title="注册帐号" target="_blank">注册</a>
+    </div>
   </div>
 </template>
 
@@ -50,6 +58,7 @@
     name: "top_head",
     data() {
       return {
+        login: true,
         activeIndex: 'home',
         showImg: true,
         show: false,
@@ -93,16 +102,18 @@
   float: left;
   margin: 1em 10% 0px 10%;
 }
+
 .in_header{
   display: inline-flex;
   float: left;
 }
+
 .right_header{
   display: flex;
   float: right;
   position: relative;
   top: -60px;
-  right: 235px;
+  right: 315px;
 }
 
 .daka{
@@ -115,16 +126,19 @@
   padding: 0px 20px;
   margin-top: 17px;
 }
+
 .find_right{
   width: 100%;
   float: right;
 }
+
 .find_set{
   width: 320px;
   padding: 10px 15px;
   position: relative;
   right: 20px;
 }
+
 .btn_search{
   position: absolute;
   right: 15px;
@@ -139,7 +153,52 @@
 }
 
 .my{
-  margin-right: 10%;
+  margin-right: 200px;
   float: right;
+}
+
+.login-out{
+  padding: 17px 0;
+  color: #c9c9c9;
+  float: right;
+  position: relative;
+  top: -60px;
+  right: 236px;
+}
+
+.login-out a{
+  display: inline-block;
+  line-height: 14px;
+}
+
+.weibo-login, .qq-login, .weixin-login{
+  margin-right: 6px;
+  width: 26px;
+  height: 26px;
+  background-position: 0 -50px!important;
+  overflow: hidden;
+  vertical-align: -7px;
+  background: url(https://css.mafengwo.net/images/header-sprites15@2x.png) no-repeat;
+  background-size: 200px;
+}
+
+.qq-login {
+  background-position: -30px -50px!important;
+}
+
+.weixin-login {
+  background-position: 0 -165px!important;
+}
+
+.split {
+  margin: 0 10px;
+  vertical-align: 3px;
+}
+
+a {
+  background-color: transparent;
+  text-decoration: none;
+  color: #ff9d00;
+  cursor: pointer;
 }
 </style>
